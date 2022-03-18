4f6c42c768d9384e3c9ac92ed4ac7b927ff9fb90
LuckyBulgur <uguraydogan37@gmail.com>
2022-03-19 00:15:44 +0100

    gg

--- fivem-dashboard/next.config.js
+++ fivem-dashboard/next.config.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const nextConfig = {
   publicRuntimeConfig: {
     serverUrl: (process.env.NODE_ENV == "development") ? 'http://localhost:3001' : process.env.SERVER_URL,
